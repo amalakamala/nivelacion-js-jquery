31bcfc80c00b5dd9a8259569e1e5093100792c8f
amalakamala <amalakamala.a@gmail.com>
2017-07-10 14:29:51 -0400

    Etapa 9 efecto de botones recipe y make it

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -4,11 +4,15 @@ $(document).ready( function(){
 	$(".js-show-recipe").click(function(event) {
 		console.log("js-show-recipe");
 		$(".page.recipe").removeClass('make');	
+		$(this).addClass('active');
+		$(".js-show-make").removeClass('active');
 	});
 
 	$(".js-show-make").click(function(event) {
 		console.log("js-show-make");
 		$(".page.recipe").addClass('make');
+		$(this).addClass('active');
+		$(".js-show-recipe").removeClass('active');
 	});
 
 });
