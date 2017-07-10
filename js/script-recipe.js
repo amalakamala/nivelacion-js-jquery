26e26fc4df32e16194fdfa2d0c5d45d4a768913a
amalakamala <amalakamala.a@gmail.com>
2017-07-10 14:25:59 -0400

    Etapa 8 btn clases js-show-recipe y js-show-make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,3 +1,14 @@
 $(document).ready( function(){
 	console.log("hola");
+
+	$(".js-show-recipe").click(function(event) {
+		console.log("js-show-recipe");
+		$(".page.recipe").removeClass('make');	
+	});
+
+	$(".js-show-make").click(function(event) {
+		console.log("js-show-make");
+		$(".page.recipe").addClass('make');
+	});
+
 });
